3108a0cbceb35dd2f39e2cb97db610290eda3407
Adam Pietrzak <adampietrzak@Adams-MacBook-Air.local>
2021-02-25 19:01:50 +0100

    Added js files

--- js/script.js
+++ js/script.js
@@ -4,27 +4,38 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  event.preventDefault();
+  const clickedElement = this;
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+  
 
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post');
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
-
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+  
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const targetArticle = document.querySelector('articleSelector');
+  console.log(targetArticle);
+  
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+  
 }
 
 const links = document.querySelectorAll('.titles a');
